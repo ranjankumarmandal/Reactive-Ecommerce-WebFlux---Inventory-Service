0746acb40a832e5035c5f0abd19cf94f68f512d0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-01 22:44:05 +0530

    Reactive Ecommerce WebFlux: Inventory Service: started working for entity layer: added all the respective annotations for entity inventory inside springboot weblux environment

--- src/main/java/com/ranjan/inventory_service/entity/Inventory.java
+++ src/main/java/com/ranjan/inventory_service/entity/Inventory.java
@@ -1,4 +1,13 @@
 package com.ranjan.inventory_service.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("inventory")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inventory {
 }
