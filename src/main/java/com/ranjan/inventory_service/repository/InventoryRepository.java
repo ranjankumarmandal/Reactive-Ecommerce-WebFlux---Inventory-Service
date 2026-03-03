88e558d5c18352f21e8d21a0b10fea6eb75bfcf9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-03 20:47:27 +0530

    Reactive Ecommerce WebFlux: started working for InventoryRepository.java: created a repository layer method - findByProductId(Long productId)

--- src/main/java/com/ranjan/inventory_service/repository/InventoryRepository.java
+++ src/main/java/com/ranjan/inventory_service/repository/InventoryRepository.java
@@ -2,6 +2,8 @@ package com.ranjan.inventory_service.repository;
 
 import com.ranjan.inventory_service.entity.Inventory;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface InventoryRepository extends ReactiveCrudRepository<Inventory, Long> {
+    Mono<Inventory> findByProductId(Long productId);
 }
