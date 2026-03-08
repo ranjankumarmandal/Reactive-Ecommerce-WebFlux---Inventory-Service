cee53b23a96b9ee7fb1ab448743aea6fdb7a679a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-08 22:19:55 +0530

    Reactive ECommerce WebFlux: working on controller layer dev changes: added respective springboot + webflux annotations and completed contructor dependency injection with final & @RequiredArgsConstructor

--- src/main/java/com/ranjan/inventory_service/controller/InventoryController.java
+++ src/main/java/com/ranjan/inventory_service/controller/InventoryController.java
@@ -1,6 +1,15 @@
 package com.ranjan.inventory_service.controller;
 
+import com.ranjan.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+    private final InventoryService inventoryService;
+
 
 }
