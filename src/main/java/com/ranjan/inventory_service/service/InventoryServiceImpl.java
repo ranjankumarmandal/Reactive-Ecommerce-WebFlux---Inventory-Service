7b08da16f8704f7890c9ebd7e441d8c7d9321432
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-08 22:11:17 +0530

    Reactive Ecommerce WebFlux: working on servie layer dev changes: added inventoryRepository inside InventoryServiceImpl.java class

--- src/main/java/com/ranjan/inventory_service/service/InventoryServiceImpl.java
+++ src/main/java/com/ranjan/inventory_service/service/InventoryServiceImpl.java
@@ -1,4 +1,7 @@
 package com.ranjan.inventory_service.service;
 
+import com.ranjan.inventory_service.repository.InventoryRepository;
+
 public class InventoryServiceImpl implements InventoryService{
+    private InventoryRepository inventoryRepository;
 }
