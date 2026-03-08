6a2752a2cd223505e731ac60730a64640fc15f3d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-08 22:07:26 +0530

    Ecommerce Reactive WebFlux: started working for service layer dev changes: completed working for inteface InventoryService.java

--- src/main/java/com/ranjan/inventory_service/service/InventoryService.java
+++ src/main/java/com/ranjan/inventory_service/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.ranjan.inventory_service.service;
 
 import com.ranjan.inventory_service.entity.Inventory;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface InventoryService {
     Mono<Inventory> saveInventory(Inventory inventory);
-
+    Flux<Inventory> getAllInventory();
+    Mono<Inventory> getInventoryByProductId(Long productId);
+    Mono<Boolean> isInStock(Long productId, Long quantity);
 }
